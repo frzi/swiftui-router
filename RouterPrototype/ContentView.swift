7b34f36ddee0758c9f10599c64988c59c86f7092
Freek <freek@appstudio.nl>
2019-08-14 00:41:18 +0200

    Initial Commit

---
+++ RouterPrototype/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RouterPrototype
+//
+//  Created by Freek Zijlmans on 14/08/2019.
+//  Copyright © 2019 Freek Zijlmans. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
